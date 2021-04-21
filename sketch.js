dd7208963f8b971d8ea2be13c5286278e6455ae4
pawanimittal312 <78547414+pawanimittal312@users.noreply.github.com>
2021-04-21 19:10:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,72 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var bgImage;
+
+var bg ;
+
+function preload() {
+    // create getBackgroundImg( ) here
+    getBackgroundImg()
+}
+
+function setup(){
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
+
+}
+
+function draw(){
+
+    // add condition to check if any background image is there to add
+  if(bgImage)
+
+background(bgImage)
+
+    Engine.update(engine);
+    // write code to display time in correct format here
+
+}
+
+async function getBackgroundImg(){
+
+    // write code to fetch time from API
+    var response=await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata")
+    var responsejson=await response.json();
+    //change the data in JSON format
+     var datetime=responsejson.datetime;
+     //console.log(datatime)
+    // write code slice the datetime
+    var hour=datetime.slice(11,13);
+
+    // add conditions to change the background images from sunrise to sunset
+  if(hour>=04 && hour<=06){
+     bg="sunrise1.png" 
+  }else if(hour>=06 && hour<=08){
+      bg="sunrise2.png"
+  }else if(hour>=08 && hour<=10){
+      bg="sunrise3.png"
+  }else if(hour>=10 && hour<=12){
+      bg="sunrise4.png"
+  }else if(hour>=12 && hour<=14){
+    bg="sunrise5.png"
+}else if(hour>=14 && hour<=16){
+    bg="sunrise6.png"
+}else if(hour>=16 && hour<=18){
+    bg="sunset7.png"
+}else if(hour>=18 && hour<=20){
+    bg="sunset8.png"
+}else if(hour>=20 && hour<=22){
+    bg="sunset9.png"
+}else if(hour>=22 && hour<=24){
+    bg="sunset10.png"
+}
+  
+    //load the image in backgroundImg variable here
+     bgImage=loadImage(bg)
+     
+}
